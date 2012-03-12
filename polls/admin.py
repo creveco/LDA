e0c6b51acd9c79153b746fb513d512a6d0e41bc2
creveco <creveco@gmail.com>
2012-03-12 15:43:44 -0400

    Funcionando bien

--- polls/admin.py
+++ polls/admin.py
@@ -2,8 +2,21 @@ from polls.models import Poll
 from polls.models import Choice
 from django.contrib import admin
 
-class PollAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question']
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
 
+   
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('question', 'pub_date')
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+
+admin.site.register(Choice) 
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
+
